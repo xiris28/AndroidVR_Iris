2da625881a5fd6c3598625e157aa3691d2d095f4
xiris28 <irisgiuliani812@gmail.com>
2020-10-03 16:13:18 -0300

    etapa 3

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -7,6 +7,15 @@ public class playerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float JumpHeigh = 3f;
+
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +26,27 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeigh * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
