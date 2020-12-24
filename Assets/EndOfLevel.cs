1255ae7d4af0b6e34d867abbfa626229e8a060fd
xiris28 <irisgiuliani812@gmail.com>
2020-12-24 16:44:35 -0300

    etapa 9 (build)

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+    public void EndOfGame()
+    {
+        //Debug.Log("HIT");
+        Application.Quit();
+        //SceneManager.LoadScene("Fps");
+    }
+}
---
+++ Assets/RotateCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float spinForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinForce * Time.deltaTime, 0);
+    }
+
+}
